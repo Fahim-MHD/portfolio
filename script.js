152d7049eaef9b007a1e0f1a7c6626a9fea0bc66
Fahim-MHD <fahimmahmood94@gmail.com>
2026-03-30 14:51:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var typed = new Typed(".typing", {
+
+strings: ["Web Developer", "CSE Student", "Frontend Developer"],
+
+typeSpeed:100,
+
+backSpeed:60,
+
+loop:true
+
+});
+
+VanillaTilt.init(document.querySelectorAll(".project,.card"), {
+
+max:25,
+
+speed:400
+
+});
